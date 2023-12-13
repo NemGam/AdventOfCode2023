7c8b9d6b335604be5461a637a2d1b87fcf706ba1
NemarioGit <Krukhmalev02@gmail.com>
2023-12-13 00:46:34 -0500

    Optimized by over 300% of Day 6 Task 2

--- Day6-Wait_For_It/Task2.py
+++ Day6-Wait_For_It/Task2.py
@@ -1,9 +1,9 @@
+import math
 
 with open("./input.txt") as f:
     time = int("".join(f.readline()[6:]).replace(' ', ''))
     distance = int("".join(f.readline()[11:]).replace(' ', ''))
-    res = 0
-    for i in range(time):
-        if (time - i) * i > distance:
-            res += 1
-    print(res)
+
+    start = (time - math.sqrt((time**2 - distance * 4))) / 2
+    end = int(start + 1)
+    print(time - 2*end + 1)
